40fd24c5dde431120cba522e339f810f76d48c3d
Omar Genao <omargenao78@gmail.com>
2025-07-23 00:43:17 -0400

    cracion accion create y edit y vista create

--- Controllers/PaisesController.cs
+++ Controllers/PaisesController.cs
@@ -1,8 +1,10 @@
 ﻿using AgenciaDeToursRD.Data;
+using AgenciaDeToursRD.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
 
 namespace AgenciaDeToursRD.Controllers
 {
@@ -35,7 +37,6 @@ namespace AgenciaDeToursRD.Controllers
 
             var pais = _context.Paises
                 .Include(p => p.Destinos)
-                .ThenInclude(d => d.Tours)
                 .FirstOrDefault(p => p.ID == id);
 
             if (pais == null)
@@ -47,37 +48,103 @@ namespace AgenciaDeToursRD.Controllers
         // GET: DestinosController/Create
         public ActionResult Create()
         {
-            return View();
+            var pais = new Pais
+            {
+                Destinos = new List<Destino>() // <- importante
+            };
+            return View(pais);
         }
 
         // POST: DestinosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Pais pais)
         {
             try
             {
+                var newPais = new Pais
+                {
+                    Nombre = pais.Nombre.ToUpper(),
+                    Destinos = new List<Destino>()
+                };
+
+                if (_context.Paises.Any(p => p.Nombre.Equals(pais.Nombre)))
+                {
+                    ModelState.AddModelError(string.Empty, $"El pais {pais.Nombre} ya existe");
+                    return View(newPais);
+                }
+
+                foreach (var destino in pais.Destinos)
+                {
+                    var d  = _context.Destinos.FirstOrDefault(d => d.Nombre == destino.Nombre.ToUpper());
+
+                    if (d != null)
+                    {
+                        ModelState.AddModelError("Destinos", $"El destino '{destino.Nombre}' ya existe en la base de datos.");
+                        return View(pais);
+                    }
+
+                    var existing = newPais.Destinos.FirstOrDefault(d => d.Nombre == destino.Nombre.ToUpper());
+
+                    if(existing != null)
+                    {
+                        ModelState.AddModelError("Destinos", $"El destino '{destino.Nombre}' está duplicado en la lista.");
+                        return View(pais);
+                    }
+
+                    newPais.Destinos.Add(new Destino
+                    {
+                        Nombre = destino.Nombre.ToUpper(),
+                        DuracionTexto = destino.DuracionTexto.ToUpper()
+                    });
+                }
+                _context.Paises.Add(newPais);
+                _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch(Exception ex)
             {
-                return View();
+                ModelState.AddModelError(string.Empty, "Debes agregar al menos un destino al Pais");
+               
             }
+            return View(pais);
         }
 
         // GET: DestinosController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            if (id <= 0)
+                ModelState.AddModelError(string.Empty, "Id invalido");
+
+            var pais = _context.Paises
+                .Include(p => p.Destinos)
+                .FirstOrDefault(p => p.ID == id);
+
+            if (pais == null)
+                ModelState.AddModelError(string.Empty, $"No existe pais con el id {id}");
+
+            return View(pais);
         }
 
         // POST: DestinosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Pais paisRequest)
         {
             try
             {
+                var pais = _context.Paises
+               .Include(p => p.Destinos)
+               .FirstOrDefault(p => p.ID == id);
+
+                if (pais == null)
+                    ModelState.AddModelError(string.Empty, $"No existe pais con el id {id}");
+
+
+                if (_context.Paises.Any(p => p.Nombre.Equals(paisRequest.Nombre) && p.ID != paisRequest.ID))
+                    ModelState.AddModelError(string.Empty, $"El pais {paisRequest.Nombre} ya existe");
+                
+
                 return RedirectToAction(nameof(Index));
             }
             catch
